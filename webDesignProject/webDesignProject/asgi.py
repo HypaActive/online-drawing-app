aced96db03be111179f2b069c1db5622d8e837ed
HypeActive <schlange@posteo.de>
2022-06-19 16:45:16 +0200

    Project rename

--- webDesignProject/webDesignProject/asgi.py
+++ webDesignProject/webDesignProject/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for onlineDrawingApp project.
+ASGI config for webDesignProject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onlineDrawingApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webDesignProject.settings')
 
 application = get_asgi_application()
--- webDesignProject/webDesignProject/wsgi.py
+++ webDesignProject/webDesignProject/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for onlineDrawingApp project.
+WSGI config for webDesignProject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onlineDrawingApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webDesignProject.settings')
 
 application = get_wsgi_application()
