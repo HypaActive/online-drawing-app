7a6643f4a9277c101ba796e608dc6feddd5a7dab
HypeActive <schlange@posteo.de>
2022-06-27 20:05:19 +0200

    Benutze onlineDrawingApp als default view

--- webDesignProject/webDesignProject/urls.py
+++ webDesignProject/webDesignProject/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from onlineDrawingApp import views as main_view
 #from webDesignProject import onlineDrawingApp
 
 urlpatterns = [
+    path('', main_view.index),
     path('admin/', admin.site.urls),
 #    path('token/'),
     path('onlineDrawingApp/', include('onlineDrawingApp.urls')),
