d362e54be4a68e20860aeb6736e73a5a7aa69500
Azgerrrrr <you@example.com>
2022-08-11 13:32:44 +0200

    adjusted settings.py and HowTo

--- webDesignProject/webDesignProject/settings.py
+++ webDesignProject/webDesignProject/settings.py
@@ -21,12 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = 'django-insecure-@$da)87u&_!6p95pomkm^h061(075*7fl+%n^nc53zj0!^^a_#'
+# SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DJANGO_DEBUG_MODE')
+DEBUG = True
+# DEBUG = os.getenv('DJANGO_DEBUG_MODE')
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', os.getenv('DJANGO_ALLOWED_HOSTS')]
+ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['127.0.0.1', 'localhost', os.getenv('DJANGO_ALLOWED_HOSTS')]
 
 
 # Application definition
